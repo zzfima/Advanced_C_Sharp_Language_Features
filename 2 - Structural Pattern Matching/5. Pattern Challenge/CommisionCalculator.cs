53298dac9f38b05439bda7fc6b69ef2b0fad0edc
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-01-28 09:10:06 +0200

    Challenge Pattern change text

--- 2 - Structural Pattern Matching/5. Pattern Challenge/CommisionCalculator.cs
+++ 2 - Structural Pattern Matching/5. Pattern Challenge/CommisionCalculator.cs
@@ -1,4 +1,15 @@
 // Code to calculate the trade commission
+// The rules:
+// A Stock trade of 0 shares should be caught and flagged as invalid
+// A Stock trade that is less than $5,000 is a 0.1% commission
+// A Stock trade that is more or equal to $5,000 is a 0.05% commission
+// Any stock trade of 1,000 shares or more is a flat fee of $10
+// Any Stock trade of 1,000 shares or more with a value of $10,000 or more is a flat fee of $5
+// A Bond trade of 5 years duration is $20, or $15 if the total trade value is $10,000 or more
+// Any bond trade of 10 years duration is $12
+// A Bond trade of 20 years duration is $10, or $5 if the total value is $5,000 or more
+// A Bond trade of any other duration should be caught and flagged as invalid
+
 public class CommissionCalculator
 {
     public static decimal CalculateTradeCommission(SecuritiesTrade trade) => trade switch
--- 2 - Structural Pattern Matching/5. Pattern Challenge/Program.cs
+++ 2 - Structural Pattern Matching/5. Pattern Challenge/Program.cs
@@ -1,18 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// The rules:
-// A Stock trade of 0 shares should be caught and flagged as invalid
-// A Stock trade that is less than $5,000 is a 0.1% commission
-// A Stock trade that is more or equal to $5,000 is a 0.05% commission
-// Any stock trade of 1,000 shares or more is a flat fee of $10
-// Any Stock trade of 1,000 shares or more with a value of $10,000 or more is a flat fee of $5
-// A Bond trade of 5 years duration is $20, or $15 if the total trade value is $10,000 or more
-// Any bond trade of 10 years duration is $12
-// A Bond trade of 20 years duration is $10, or $5 if the total value is $5,000 or more
-// A Bond trade of any other duration should be caught and flagged as invalid
-
-// Test code to test the trade commission calculation algorithm:
+﻿// Test code to test the trade commission calculation algorithm:
 SecuritiesTrade[] tradeList = new SecuritiesTrade[] {
     new StockTrade(){Symbol="ABCD", Quantity=1200, Price=27.81m},
     new StockTrade(){Symbol="WXYZ", Quantity=1000, Price=7.92m},
