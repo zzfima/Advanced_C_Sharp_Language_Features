ff7b96423d81a375e80caf120814c55003f0ca37
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-02-01 17:33:55 +0200

    Overriding ToString

---
+++ 3 - OOP/2. Overriding ToString/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Object methods
+    ToString
+    Еquals(Object)
+    GetHashCode
+    GetType
+    MemberwiseClone
+*/
+
+var x = 4.56f;
+var str = x.ToString();
+System.Console.WriteLine($"{str}");
+
+var p = new Person { LastName = "Abramov", Age = 33 };
+System.Console.WriteLine(p);
+var (a, fn) = p;
+System.Console.WriteLine("*** " + fn);
+
+class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}, {Age} years old";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Person p && p.Age == Age && p.FirstName == FirstName && p.LastName == LastName)
+            return true;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public void Deconstruct(out int a, out string fn) => (a, fn) = (Age, FirstName ?? "Default name");
+}
